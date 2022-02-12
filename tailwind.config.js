a8b6154aa19c2a06afb8bc007e1ff6ae6f871fe4
Burhan <2019cs626@student.uet.edu.pk>
2022-02-12 12:50:17 +0500

    Added Home Page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./pages/**/*.{html,js,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'black-prime': '#111111',
+        'light-black-prime': '#2b2a2a',
+        'tabs-black-prime': '#444',
+        'yellow-prime': '#FFB400',
+      }
+    },
+  },
+  plugins: [],
+}
